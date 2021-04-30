7d39979d9d2754f8a23471284dd902e2888feb56
cperez <chrper31@gmail.com>
2021-04-30 11:53:49 +0200

    envoi avec lien ssh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
       System.out.println("Hello OpenClassrooms");
-      System.out.println("Hello OpenClassrooms");
-      System.out.println("Hello OpenClassrooms");
-      System.out.println("Hello OpenClassrooms");
-      System.out.println("Hello OpenClassrooms");
 
     }
 }
